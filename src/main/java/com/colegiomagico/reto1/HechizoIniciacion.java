ac29a81abce6985245e1392f96d71ce1772b8c6a
JCamilo <juancamilo321312@gmail.com>
2026-09-05 16:14:18 +0000

    feat: se ha añadido el nombre de usuario y de profesor en el reto 1

--- src/main/java/com/colegiomagico/reto1/HechizoIniciacion.java
+++ src/main/java/com/colegiomagico/reto1/HechizoIniciacion.java
@@ -6,7 +6,7 @@ package com.colegiomagico.reto1;
 public class HechizoIniciacion {
 
     public static String invocar() {
-        return "El nombre es Juan Camilo y mi profesor es Daniel Urazan" ; // TODO: Cambia esto
+        return "El nombre es Juan Camilo y mi profesor es Juan José" ; // TODO: Cambia esto
     }
 
     public static void main(String[] args) {
