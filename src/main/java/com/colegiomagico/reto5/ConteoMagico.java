bab1302c0cd0f3c1bd7c63d1f6b3de007a8aaf66
dahianagal22 <galvisdahi22@gmail.com>
2026-09-09 14:33:33 -0500

    feet: se añadió la repeticion del hechizo en el reto 5, en caso de no saber cuanto se necesita

--- src/main/java/com/colegiomagico/reto5/ConteoMagico.java
+++ src/main/java/com/colegiomagico/reto5/ConteoMagico.java
@@ -6,7 +6,15 @@ package com.colegiomagico.reto5;
 public class ConteoMagico {
 
     public static int contar(int limite) {
-        return 0; // TODO: Cambia esto
+        int sumar = 0;
+        int contador = 1;
+
+        while (contador <= limite) {
+            sumar += contador;
+            contador++;
+        }
+
+        return sumar;
     }
 
     public static void main(String[] args) {
